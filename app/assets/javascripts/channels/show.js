c586d9a43905ec95004f87136ee05ca78e8b7d13
JurgenHahn <jurgenehahn@gmail.com>
2017-03-30 19:50:45 -0400

    links within modal show page working

--- app/assets/javascripts/channels/show.js
+++ app/assets/javascripts/channels/show.js
@@ -1,52 +1,50 @@
 $(document).ready(function() {
 
+  $('img').on('click', function(event) {
+    event.preventDefault();
+    $('.comic-details').fadeIn('300');
+    var link = $(this).parent('a').attr('href');
 
+    $.ajax({
+      url: link,
+      method: 'GET'
+    }).done(function(data) {
 
-    $('img').on('click', function(event) {
-        event.preventDefault();
-        $('.comic-details').fadeIn('300');
-        var link = $(this).parent('a').attr('href');
-
-        $.ajax({
-            url: link,
-            method: 'GET'
-          }).done(function(data){
-
-            $('.comic-details').css('display', 'block');
-            $('.comic-details').html(data);
-
-
-          });
-
-
-          $('.next-link').on('click', function(event){
-            event.preventDefault();
-            });
-
-            $('body').on('click', function(event){
-              $('.comic-details').css('display', 'none')
-
-            });
-
+      $('.comic-details').css('display', 'block');
+      $('.comic-details').html(data);
     });
+  });
 
+  $('.comic-details').on('click', '.next-link', function(event) {
+    event.stopPropagation();
+    event.preventDefault();
+    var nextLink = $('a.next-link').attr('href');
 
-      // var showLink = $('a.next-link').attr('href');
-      //
-      // $.ajax({
-      //   url: showLink,
-      //   method: 'GET'
-      // }).done(function(data){
-      //
-      //   $('.comic-details').css('display', 'block');
-      //   $('.comic-details').html(data);
-      //
-      //   $('.comic-details').on('click', function(){
-      //     $('.comic-details').css('display', 'none')
-      //
-      //   });
+    $.ajax({
+      url: nextLink,
+      method: 'GET'
+    }).done(function(data) {
+      $('.comic-details').css('display', 'block');
+      $('.comic-details').html(data);
+    });
+  });
 
-      // });
+  $('.comic-details').on('click', '.previous-link', function(event) {
+    event.stopPropagation();
+    event.preventDefault();
+    var previousLink = $('a.previous-link').attr('href');
 
+    $.ajax({
+      url: previousLink,
+      method: 'GET'
+    }).done(function(data) {
+      $('.comic-details').css('display', 'block');
+      $('.comic-details').html(data);
+    });
+  });
+
+  $('body').on('click', function(event) {
+    $('.comic-details').css('display', 'none')
+  });
 
 });
