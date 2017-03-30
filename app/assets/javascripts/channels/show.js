5ae48cc714a0601f1fabedf5d00c8f038d75315c
JurgenHahn <jurgenehahn@gmail.com>
2017-03-30 18:12:52 -0400

    added modal show pages

--- app/assets/javascripts/channels/show.js
+++ app/assets/javascripts/channels/show.js
@@ -1,16 +1,52 @@
 $(document).ready(function() {
 
+
+
     $('img').on('click', function(event) {
         event.preventDefault();
-
+        $('.comic-details').fadeIn('300');
         var link = $(this).parent('a').attr('href');
+
         $.ajax({
             url: link,
             method: 'GET'
           }).done(function(data){
 
-              console.log(data);
+            $('.comic-details').css('display', 'block');
+            $('.comic-details').html(data);
+
 
           });
+
+
+          $('.next-link').on('click', function(event){
+            event.preventDefault();
+            });
+
+            $('body').on('click', function(event){
+              $('.comic-details').css('display', 'none')
+
+            });
+
     });
+
+
+      // var showLink = $('a.next-link').attr('href');
+      //
+      // $.ajax({
+      //   url: showLink,
+      //   method: 'GET'
+      // }).done(function(data){
+      //
+      //   $('.comic-details').css('display', 'block');
+      //   $('.comic-details').html(data);
+      //
+      //   $('.comic-details').on('click', function(){
+      //     $('.comic-details').css('display', 'none')
+      //
+      //   });
+
+      // });
+
+
 });
