d6975dce1555806d0bdc2c7bc30cbe0eb4b1bc47
JurgenHahn <jurgenehahn@gmail.com>
2017-08-15 10:30:50 -0400

    added logic to allow search new stories table

--- app/models/comic.rb
+++ app/models/comic.rb
@@ -1,24 +1,22 @@
 class Comic < ApplicationRecord
 
   has_many :stories, dependent: :destroy
-  
-  validates :volume, format: { with: /[1-5]/,
-    message: "must from 1..5"}
 
   def self.list_comics
     Comic.all.order(:volume).order(:issue)
   end
 
   def self.search(search)
-    where("cover_artists LIKE ?
-      OR writers LIKE ?
-      OR pencilers LIKE ?
-      OR inkers LIKE ?
-      OR colourists LIKE ?
-      OR letterers LIKE ?
-      OR editors LIKE ?
-      OR editor_in_chief LIKE ?
-      OR tags LIKE ?",
+    Comic.joins('LEFT JOIN stories ON comics.id = stories.comic_id')
+    .where("writers LIKE ? OR
+            pencilers LIKE ? OR
+            inkers LIKE ? OR
+            colourists LIKE ? OR
+            letterers LIKE ? OR
+            editors LIKE ? OR
+            cover_artists LIKE ? OR
+            editor_in_chief LIKE ? OR
+            tags LIKE ?",
         "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%",
         "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%")
     end
